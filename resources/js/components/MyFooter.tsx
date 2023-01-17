2310bacff675e9e209ffc166a626f60494156c11
Patryk (PsychoX) Ludwikowski <patryk.ludwikowski.7@gmail.com>
2023-01-17 04:21:43 +0100

    Nicer footer

--- resources/js/components/MyFooter.tsx
+++ resources/js/components/MyFooter.tsx
@@ -1,12 +1,39 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Nav, Row } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+const FooterLink = (props: any) => {
+	return <Nav.Link className="px-0 py-1 text-muted" {...props}>{props.children}</Nav.Link>
+}
 
 const MyFooter = () => {
-	return <footer className="mt-auto">
+	return <footer className="mt-auto pt-2 shadow">
 		<Container>
-			<address className="text-center fw-light mb-0">
-				Przygotowane przez <a href="mailto:patryk.ludwikowski.7@gmail.com" className="text-decoration-none">Patryka Ludwikowskiego</a> w ramach zaliczenia przedmiotu Aplikacje Internetowe, Uniwersytet Rzeszowski 2022.
-			</address>
+			<Row>
+				<Col xs={6} md={3}>
+					<h5>Linki</h5>
+					<FooterLink as={NavLink} to="/">Strona główna</FooterLink>
+					<FooterLink as={NavLink} to="/games/">Katalog gier</FooterLink>
+					<FooterLink as={NavLink} to="/users/">Gracze</FooterLink>
+					<FooterLink as={NavLink} to="/about/">O speedranowaniu</FooterLink>
+				</Col>
+				<Col xs={6} md={3}>
+					<h5>Odwiedź nas</h5>
+					<FooterLink href="#"><i className="bi bi-discord"></i> Discord</FooterLink>
+					<FooterLink href="#"><i className="bi bi-twitter"></i> Twitter</FooterLink>
+					<FooterLink href="#"><i className="bi bi-youtube"></i> YouTube</FooterLink>
+					<FooterLink href="#"><i className="bi bi-twitch"></i> Twitch</FooterLink>
+				</Col>
+				<Col xs={12} md={6}>
+					<div className="hstack gap-2 flex-wrap mb-2">
+						<img src="/logo.png" alt="logo" className="shadow-sm" style={{display: "inline", height: "4em"}}/>
+						<h5 className="footer-brand mb-0">SpeedrunsApp</h5>
+					</div>
+					<address className="fw-light mb-0 text-muted text-center text-md-start">
+						Przygotowane przez <a href="mailto:patryk.ludwikowski.7@gmail.com" className="text-decoration-none">Patryka Ludwikowskiego</a> w ramach zaliczenia przedmiotu Aplikacje Internetowe, Uniwersytet Rzeszowski 2022 oraz Projektowanie Interfejsów Internetowych, Uniwersytet Rzeszowski 2023.
+					</address>
+				</Col>
+			</Row>
 		</Container>
 	</footer>
 };
